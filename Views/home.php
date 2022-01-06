515f5bfe379d6bb40663db4e3c2d9fd6bcabe29d
f04k04369 <f04k04369@gmail.com>
2022-01-06 22:18:56 +0900

    会員登録＆ログイン画面

--- Views/home.php
+++ Views/home.php
@@ -96,18 +96,28 @@ function convertToDayTimeAgo(string $datetime)
 ?>
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="<?php echo HOME_URL;?>Views/img/logo-twitterblue.svg">
-    <!-- Bootstrap CSS -->
+<!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+<!--css呼び出し-->
     <link rel="stylesheet" href="<?php echo HOME_URL;?>Views/css/style.css">
-
+<!-- JS -->
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous" defer></script>
+<!-- JavaScript Bundle with Popper このブートストラップはこのjqueryに依存-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous" defer></script>
+<!--いいね！JS-->
+<script src="<?php echo HOME_URL; ?>Views/js/likes.js" defer></script>
+<!--タイトル表示-->
     <title>ホーム画面 / Twitterクローン</title>
     <meta name="description" content="ホーム画面です">
 </head>
+
+<!--表示部分作成-->
 <body class="home">
     <div class="container">
         <div class="side">
@@ -119,7 +129,10 @@ function convertToDayTimeAgo(string $datetime)
                     <li class="nav-item"><a href="notification.php" class="nav-link"><img src="<?php echo HOME_URL;?>Views/img/icon-notification.svg" alt=""></a></li>
                     <li class="nav-item"><a href="profile.php" class="nav-link"><img src="<?php echo HOME_URL;?>Views/img/icon-profile.svg" alt=""></a></li>
                     <li class="nav-item"><a href="post.php" class="nav-link"><img src="<?php echo HOME_URL;?>Views/img/icon-post-tweet-twitterblue.svg" class="post-tweet" alt=""></a></li>
-                    <li class="nav-item my-icon"><img src="<?php echo HOME_URL;?>Views/img_uploaded/user/sample-person.jpg" alt=""></li>
+                    <li class="nav-item my-icon"><img src="<?php echo HOME_URL;?>Views/img_uploaded/user/sample-person.jpg" alt="" class="js-popover"
+                    data-bs-container="body" data-bs-toggle="popover" data-bs-placement="right" data-bs-html="true"
+                    data-bs-content="<a href='profile.php'>プロフィール</a><br><a href='sign-out.php'>ログアウト</a>"
+                    ></li>
                 </ul>
             </div>
         </div>
@@ -178,7 +191,7 @@ function convertToDayTimeAgo(string $datetime)
                             <?php endif; ?>
 
                                 <div class="icon-list">
-                                <div class="like">
+                                <div class="like js-like" data-like-id="<?php echo htmlspecialchars($view_tweet['like_id']); ?>">
                                     <?php
                                     if (isset($view_tweet['like_id'])){
                                         //いいね！している場合、青のハートを表示
@@ -189,7 +202,7 @@ function convertToDayTimeAgo(string $datetime)
                                     }
                                     ?>
                                 </div>
-                                <div class="like-count"><?php echo htmlspecialchars($view_tweet['like_count']); ?></div>
+                                <div class="like-count js-like-count"><?php echo htmlspecialchars($view_tweet['like_count']); ?></div>
                             </div>
                         </div>
                     </div>
@@ -199,5 +212,11 @@ function convertToDayTimeAgo(string $datetime)
         <?php endif;?>
         </div>
     </div>
+<!--javascript(javascriptを書くときは、極力bodyの閉じタグの上に書くこと)-->
+<script>
+    document.addEventListener('DOMContentLoaded',function(){
+        $('.js-popover').popover();
+    },false);
+</script>
 </body>
 </html>
