3b9190451188332f9f6f7885473d6fce7da77016
f04k04369 <f04k04369@gmail.com>
2022-01-19 01:26:24 +0900

    つぶやき投稿機能

--- util.php
+++ util.php
@@ -122,10 +122,43 @@ $user = $_SESSION['USER'];
 if (!isset($user['image_name'])){
     $user['image_name'] = null;
 }
-$user['iamge_path'] = buildImagePath($user['image_name'], 'user');
-
+$user['image_path'] = buildImagePath($user['image_name'], 'user');
 return $user;
+}
 
+/**
+ * 画像をアップロード
+ * 
+ * @param array $user
+ * @param array $file
+ * @param string $type
+ * @return string 画像のファイル名
+ */
+function uploadImage(array $user, array $file, string $type){
+
+    //画像のファイル名から拡張子を取得（例:.png)
+    $image_extension = strrchr($file['name'], '.');
+
+    //画像のファイル名を作成（YmdHis:2021-01-01 00:00:00 ならば 20210101000000）
+
+    $image_name = $user['id'] . '_' . date('YmdHis') . $image_extension;
+
+    //保存先のディレクトリ
+    $directory = '../Views/img_uploaded/' . $type . '/';
+
+    //画像のパス
+    $image_path = $directory . $image_name;
+
+    //画像を設置
+    move_uploaded_file($file['tmp_name'], $image_path);
+
+    //画像ファイルの場合->ファイル名をreturn
+    if (true){
+        return $image_name;
+    }
+    //画像ファイル以外の場合
+    echo '選択されたファイルが画像ではないため処理を停止しました';
+    exit;
 }
 
 ?>
