d3c30349dbc4bfad8a0b5a354438629b19c3849a
f04k04369 <f04k04369@gmail.com>
2022-03-02 22:43:29 +0900

    ホーム機能をタイムライン化

--- Controllers/home.php
+++ Controllers/home.php
@@ -10,6 +10,8 @@ include_once '../util.php';
 
 //ツイートデータ操作モデルを読み込む
 include_once '../Models/tweets.php';
+// フォローデータ操作モデルを読み込む
+include_once '../Models/follows.php';
 
 //TODO:ログインチェック
     $user = getUserSession();
@@ -18,13 +20,18 @@ include_once '../Models/tweets.php';
         header('Location:' . HOME_URL . 'Controllers/sign-in.php');
         exit;
     }
+    // 自分がフォローしているユーザーID一覧を取得
+    $following_user_ids = findFollowingUserIds($user['id']);
+
+    // 自分のツイートも表示するために自分のIDも追加
+    $following_user_ids[] = $user['id'];
 
 //表示用の変数
 $view_user = $user;
 
 //ツイート一覧
 //モデルから取得するよう変更
-$view_tweets = findTweets($user);
+$view_tweets = findTweets($user, null, $following_user_ids);
 
 //画面表示
 include_once'../Views/home.php';
